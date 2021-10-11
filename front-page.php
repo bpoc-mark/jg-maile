8ae9e8da4d799f5727a1f37863894bd9d6e49717
arsua-greg <gregarsua@bpoc.co.jp>
2021-10-11 23:25:58 +0800

    Added: sect_3 & sect_4

--- front-page.php
+++ front-page.php
@@ -10,4 +10,55 @@
 <div class="sect_2">
 
 </div>
+<div class="sect_3">
+    <div class="l-wrap">
+        <div class="boxes">
+            <div class="box">
+                <div class="icon">
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/top/sect_3_icon-1.svg" alt="">
+                </div>
+                <p class="box__ttl">まいれとは？</p>
+                <p class="box__desc">テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります</p>
+                <div class="box__btn">
+                    <p>詳しくはこちら</p>
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/top/arrow_r.svg" alt="">
+                </div>
+            </div>
+            <div class="box">
+                <div class="icon">
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/top/sect_3_icon-2.svg" alt="">
+                </div>
+                <p class="box__ttl">社内研修</p>
+                <p class="box__desc">テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります</p>
+                <div class="box__btn">
+                    <p>詳しくはこちら</p>
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/top/arrow_r.svg" alt="">
+                </div>
+            </div>
+            <div class="box">
+                <div class="icon">
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/top/sect_3_icon-3.svg" alt="">
+                </div>
+                <p class="box__ttl">理　念</p>
+                <p class="box__desc">テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります</p>
+                <div class="box__btn">
+                    <p>詳しくはこちら</p>
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/top/arrow_r.svg" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="event">
+            <img src="<?php echo get_template_directory_uri(); ?>/release/image/top/sect_3_event.png" alt="">
+        </div>
+    </div>
+</div>
+
+<div class="sect_4">
+    <div class="l-wrap">
+        <div class="sect_4__ttl">
+            employment information
+        </div>
+        <img src="<?php echo get_template_directory_uri(); ?>/release/image/top/sect_4_divider.svg" alt="">
+    </div>
+</div>
 <?php get_footer(); ?>
