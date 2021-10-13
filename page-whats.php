61453e7a6b4c646e8d52a6afcf6cc7a3c0ecabed
arsua-greg <gregarsua@bpoc.co.jp>
2021-10-13 22:06:32 +0800

    Add: page-whats

---
+++ page-whats.php
@@ -0,0 +1,52 @@
+<?php get_header(); ?>
+<div class="p-banner">
+    <div class="l-wrap">
+        <div class="title">
+            <h1>what is the maile like?</h1>
+            <p>まいれってどんな会社？</p>
+        </div>
+        <img src="<?php echo get_template_directory_uri(); ?>/release/image/top/top_coconut.svg" alt="">
+        <img src="<?php echo get_template_directory_uri(); ?>/release/image/top/top_ttl.svg" alt="">
+        <img src="<?php echo get_template_directory_uri(); ?>/release/image/top/top_ttl_sp.svg" alt="">
+    </div>
+</div>
+<div class="page__whats">
+    <div class="l-wrap">
+        <img class="cloud" src="<?php echo get_template_directory_uri(); ?>/release/image/top/cloud_1.svg" alt="">
+        <img class="cloud" src="<?php echo get_template_directory_uri(); ?>/release/image/top/cloud_1.svg" alt="">
+        <div class="sect_1">
+            <img src="<?php echo get_template_directory_uri(); ?>/release/image/page/whats_img_1.png" alt="">
+            <p>人と人との出逢いは偶然ではなく、必然です。人は生きていく中で何かしら周りの人から援助を受けて生きています。家族、友人、職場の仲間、協力業者の方々。そんな自分に関わる全ての人達に支えられてを生きていること。自分の周りにいてくれる人達が当たり前ではないということ。<br>MAILE株式会社は、そういった自分に本気で関わって下さった人達に出逢えたことに感謝の気持ちを持ち、人と人とのご縁に感謝し、自分だけでなく、家族、友人、MAILEに関わって下さる全ての人達と、共に幸せになれる会社でありたいと願っています。</p>
+        </div>
+    </div>
+    <div class="events">
+        <img class="cloud" src="<?php echo get_template_directory_uri(); ?>/release/image/top/cloud_1.svg" alt="">
+        <img class="cloud" src="<?php echo get_template_directory_uri(); ?>/release/image/top/cloud_2.svg" alt="">
+        <div class="events__container">
+            <p class="events__ttl">
+                events
+            </p>
+            <p class="events__sub">年間行事</p>
+            <p class="events__desc">MAILE株式会社ではスタッフとその家族とのかかわりも大切にしていてコミュニケーションが図れる場を大事にしています。<br>お子さんも一緒に参加できて家族のように接することのできる環境づくりにこだわっています。</p>
+        </div>
+    </div>
+    <div class="training">
+        <div class="training__container">
+            <p class="training__ttl">training system</p>
+            <p class="training__sub">社内研修制度</p>
+
+            <div class="training__office">
+                <div class="training__office--row_1">
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/page/whats_office.png" alt="">
+                </div>
+                <div class="training__office--row_2">
+                    <p class="ttl">診療時間内研修</p>
+                    <p class="desc">毎月、全店舗を半日休診や一日休診にして、外部から講師を招き研修を行っております。基本的には全スタッフ合同で研修を行います。<br>実践的で即効性のある施術法や、患者様に痛みを聞き出す問診力などを働きながらもさらに磨きをかけるべく日々スキルアップを目指しています。</p>
+                    <p class="ttl">新人研修</p>
+                    <p class="desc">入社したての新人スタッフへは初めの1ヶ月を新人研修として、基本的な治療の修得はもちろん、会社の考え、理念などの研修を行っております。</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
