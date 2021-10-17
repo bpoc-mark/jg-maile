7f1b195160b3d5ba84d8a76fe66d0f0868fc2910
arsua-greg <gregarsua@bpoc.co.jp>
2021-10-18 00:17:15 +0800

    Add: contact page

--- page-contact.php
+++ page-contact.php
@@ -14,6 +14,11 @@
 </div>
 <div class="page_contact">
     <div class="l-wrap">
+        <div class="cont_clouds">
+            <img src="<?php echo get_template_directory_uri(); ?>/release/image/top/cloud_1.svg" alt="">
+            <img src="<?php echo get_template_directory_uri(); ?>/release/image/top/cloud_1.svg" alt="">
+            <img src="<?php echo get_template_directory_uri(); ?>/release/image/top/cloud_2.svg" alt="">
+        </div>
         <div class="page_contact__faq">
             <p class="page_contact__faq--ttl">faq</p>
             <p class="page_contact__faq--sub">よくある質問</p>
@@ -53,67 +58,78 @@
                 </div>
             </div>
         </div>
-        <div class="page_contact__contact">
-            <p class="page_contact__contact--ttl">contact</p>
-            <p class="page_contact__contact--sub">お問い合わせ</p>
+    </div>
+    <div class="page_contact__contact">
+        <p class="page_contact__contact--ttl">contact</p>
+        <p class="page_contact__contact--sub">お問い合わせ</p>
 
-            <div class="form-group">
-                <div class="row_1">
-                    <p>お名前<span>※必須</span></p>
-                </div>
-                <div class="row_2">
-                    <input type="text" placeholder="漢字でフルネームを記入してください。">
-                </div>
+        <div class="form-group">
+            <div class="row_1">
+                <p>お名前<span>※必須</span></p>
             </div>
-            <div class="form-group">
-                <div class="row_1">
-                    <p>フリガナ<span>※必須</span></p>
-                </div>
-                <div class="row_2">
-                    <input type="text">
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="row_1">
-                    <p>電話番号<span>※必須</span></p>
-                </div>
-                <div class="row_2">
-                    <input type="text">
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="row_1">
-                    <p>メールアドレス<span>※必須</span></p>
-                </div>
-                <div class="row_2">
-                    <input type="text">
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="row_1">
-                    <p>住所<span>※必須</span></p>
-                </div>
-                <div class="row_2">
-                    <input type="text" placeholder="郵便番号">
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="row_1">
-                    <p>お問い合わせ項目<span>※必須</span></p>
-                </div>
-                <div class="row_2">
-                    <input type="text" placeholder="新卒採用">
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="row_1">
-                    <p>お問い合わせ内容</p>
-                </div>
-                <div class="row_2">
-                    <textarea name="message" id="" cols="30" rows="10">ご希望や、ご質問がありましたらこちらにご入力をお願いします。資格をお持ちでしたらこちらにご入力をお願いいたします。</textarea>
-                </div>
+            <div class="row_2">
+                <input type="text" placeholder="漢字でフルネームを記入してください。">
             </div>
         </div>
+        <div class="form-group">
+            <div class="row_1">
+                <p>フリガナ<span>※必須</span></p>
+            </div>
+            <div class="row_2">
+                <input type="text">
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="row_1">
+                <p>電話番号<span>※必須</span></p>
+            </div>
+            <div class="row_2">
+                <input type="text">
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="row_1">
+                <p>メールアドレス<span>※必須</span></p>
+            </div>
+            <div class="row_2">
+                <input type="text">
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="row_1">
+                <p>住所<span>※必須</span></p>
+            </div>
+            <div class="row_2">
+                <input type="text" placeholder="郵便番号">
+                <input type="text" placeholder="郵便番号">
+                <input type="text" placeholder="郵便番号">
+                <input type="text" placeholder="郵便番号">
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="row_1">
+                <p>お問い合わせ項目<span>※必須</span></p>
+            </div>
+            <div class="row_2">
+                <input type="text" placeholder="新卒採用">
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="row_1">
+                <p>お問い合わせ内容</p>
+            </div>
+            <div class="row_2">
+                <textarea name="message" id="" cols="30" rows="10">ご希望や、ご質問がありましたらこちらにご入力をお願いします。資格をお持ちでしたらこちらにご入力をお願いいたします。</textarea>
+            </div>
+        </div>
+
+        <div class="p-details--btn_blk">
+            <a href="">
+                <p>CONFIRM</p>
+                <img src="<?php echo get_template_directory_uri(); ?>/release/image/top/arrow_r.svg" alt="">
+            </a>
+        </div>
     </div>
+
 </div>
 <?php get_footer(); ?>
